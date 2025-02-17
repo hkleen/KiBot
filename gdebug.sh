50ff7b7b012c3971eee27991c395657cf5fe7f7f
Salvador E. Tropea <salvador@inti.gob.ar>
2025-02-17 13:08:30 -0300

    [CI/CD][Blender] Try with a link to /bin/4.2/extensions/system/

--- gdebug.sh
+++ gdebug.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-whoami
-ls ~
-ls -la /root/.config/blender/4.2/extensions/blender_org/
-cat /root/.config/blender/4.2/extensions/blender_org/.blender_ext/index.json
+# whoami
+# ls ~
+# ls -la /root/.config/blender/4.2/extensions/blender_org/
+# cat /root/.config/blender/4.2/extensions/blender_org/.blender_ext/index.json
 ln -s /root/.config/blender/4.2/extensions/blender_org/pcb3d_importer /bin/4.2/extensions/system/
-ls -la /bin/4.2/extensions/system/
+# ls -la /bin/4.2/extensions/system/
 blender --online-mode --command extension list
+pytest-3 -v --test_dir=output -k test_blender_export_1
