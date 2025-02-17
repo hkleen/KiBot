d8504908ddc655521088a63d45e4568d93fcc682
Salvador E. Tropea <salvador@inti.gob.ar>
2025-02-17 13:00:17 -0300

    [CI/CD][Blender] Try to install at system level
    GitHub runs the docker image as root, but changes the HOME so root
    settings are lost

--- gdebug.sh
+++ gdebug.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 whoami
 ls ~
-ls -la ~/.config/blender/4.2/extensions/blender_org/
-cat ~/.config/blender/4.2/extensions/blender_org/.blender_ext/index.json
+ls -la /root/.config/blender/4.2/extensions/blender_org/
+cat /root/.config/blender/4.2/extensions/blender_org/.blender_ext/index.json
+ln -s /root/.config/blender/4.2/extensions/blender_org/pcb3d_importer /bin/4.2/extensions/system/
+ls -la /bin/4.2/extensions/system/
+blender --online-mode --command extension list
